b28e4416e52ec4e311d775d1d0fdfcddb37d012f
Твоё имя <>
2020-09-21 17:47:32 +0300

    Initial commit

---
+++ src/exe5/CircleMovable.java
@@ -0,0 +1,23 @@
+package exe5;
+
+public class CircleMovable extends Circle implements Movable{
+
+    public CircleMovable(Point center, double radius) {
+        super(center, radius);
+    }
+
+
+    @Override
+    public void move(int x, int y) {
+        try {
+            ((PointMovable) this.getCenter()).move(x, y);
+        }
+        catch (ClassCastException e){
+            // тупая логика через геты сеты
+            e.printStackTrace();
+            System.out.println("Я упаль");
+        }
+        //this.getCenter();
+        // this.getRadius();
+    }
+}
